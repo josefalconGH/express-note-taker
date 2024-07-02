e9e6edce627d8b9b4f66c1df6771c14779d8c40a
flcn <jfalconpriv@gmail.com>
2024-07-02 14:29:31 -0400

    added GET reqs

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ const PORT = process.env.PORT || 3001;
 // routes
 app.use(express.static('public'));
 app.use(express.json());
+
+// GET / should return the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+});
+
+// GET /notes should return the notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+});
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(readFileSync('./db/db.json', 'utf8'));
+    res.json(JSON.parse(notes));
+});
