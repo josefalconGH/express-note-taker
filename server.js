b1f864674ce0ca8fea82a7cd66b8998a58e76142
flcn <jfalconpriv@gmail.com>
2024-07-02 14:49:33 -0400

    updated GET for index.html

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const path = require('path');
 const app = express();
 
 // middleware
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 
 // routes
 app.use(express.static('public'));
 app.use(express.json());
 
 // GET / should return the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // GET /notes should return the notes.html file
